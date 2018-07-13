8049e66dc29d115d046e1c0500591bca5eb7a7e3
administrador <administrador@M72440.local>
2018-07-13 09:53:14 -0300

    Initial commit

---
+++ AppleAndroidTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  AppleAndroidTableViewCell.swift
+//  TableViewExample
+//
+//  Created by administrador on 13/07/2018.
+//  Copyright © 2018 administrador. All rights reserved.
+//
+
+import UIKit
+
+class AppleAndroidTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var appleAndroid2: UIImageView!
+    @IBOutlet weak var appleAndroid: UIImageView!
+    
+    @IBOutlet weak var labelNumeroRow: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+   
+}
--- TableViewExample/ViewController.swift
+++ TableViewExample/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableViewEx: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableViewEx?.dataSource = self
+        self.tableViewEx?.delegate = self
+        self.tableViewEx?.register(UINib(nibName: "AppleAndroidTableViewCell", bundle: nil), forCellReuseIdentifier: "cellApple")
     }
+}
 
+//MARK - TableView
+    extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath)-> CGFloat{
+            return 240
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellApple", for: indexPath) as! AppleAndroidTableViewCell
+        cell.backgroundColor = .brown
+        cell.labelNumeroRow.text = ">     \(indexPath.row)   X"
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
